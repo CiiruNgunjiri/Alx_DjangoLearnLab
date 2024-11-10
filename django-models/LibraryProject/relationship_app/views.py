de840ba2eac3a788c78bd026cd7087da516054e4
Linda <ciiru.ngunjiri@gmail.com>
2024-11-10 16:34:42 +0300

    modified files within django-models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+def list_books(request):
+    books = Book.objects.all()  # Query all books
+    return render(request, 'list_books.html', {'books': books})
+
+from django.views.generic import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'  # Specify your template name
+    context_object_name = 'library'        # Context variable name for the template
+
+    def get_queryset(self):
+        return Library.objects.prefetch_related('books')  # Optimize book retrieval
