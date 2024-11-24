0fc13c3065e0b0dbb3e78e1248cf7594f3320540
Linda <ciiru.ngunjiri@gmail.com>
2024-11-24 19:29:58 +0300

    modified files in django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser, Group, Permission
-from .managers import CustomUserManager
+from django.contrib.auth.models import AbstractUser, Group, Permission, BaseUserManager
 
 # Create your models here.
 class Book(models.Model):
@@ -30,3 +29,18 @@ class CustomUser(AbstractUser):
 class SomeModel(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='bookshelf_some_models')
 
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, password, **extra_fields)
