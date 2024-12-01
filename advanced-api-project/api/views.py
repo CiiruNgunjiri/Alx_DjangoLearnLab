c2b5a4eb685484afbaad14b9d38c55ac07f460c9
Linda <ciiru.ngunjiri@gmail.com>
2024-12-01 23:14:52 +0300

    modified views.py file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django.http import JsonResponse
 from django.views import View
 from django.shortcuts import get_object_or_404
