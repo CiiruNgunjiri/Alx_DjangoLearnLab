d8d31c63f0f5a4ba65ff94cfcbe05a6c8b78c0b4
Linda <ciiru.ngunjiri@gmail.com>
2024-12-01 22:13:17 +0300

    modified views.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,10 +2,14 @@ from django.urls import path, include
 from .views import BookViewSet, BookList
 from rest_framework.routers import  DefaultRouter
 from . import views
+from .views import CustomAuthToken
+from rest_framework.authtoken.views import obtain_auth_token
+
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    path('api-token-auth/', CustomAuthToken.as_view(), name='api-token-auth'),
     path('', include(router.urls)),  # Includes all routes registered with the router
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
     path('contact/', views.contact, name='contact'),
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class BookList(generics.ListAPIView):
@@ -11,9 +17,25 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()  # Queryset for retrieving all books
     serializer_class = BookSerializer  # Use the BookSerializer for serialization
+    permission_classes =[IsAuthenticated] #Only authenticated users can access this view set
 
 def contact(request):
     return render(request, 'contact.html')
 
 def about(request):
     return render(request, 'about.html')
+
+class CustomAuthToken(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+            if user.check_password(password):
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({'token': token.key}, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
