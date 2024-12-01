eadf2c5ae02680e4d0af417679721131dac8ac65
Linda <ciiru.ngunjiri@gmail.com>
2024-12-01 21:16:35 +0300

    modified files with api_project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookViewSet, BookList
+from rest_framework.routers import  DefaultRouter
+from . import views
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    path('', include(router.urls)),  # Includes all routes registered with the router
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
 ]
+
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -7,3 +7,13 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  # Queryset for retrieving all books
     serializer_class = BookSerializer  # Use the BookSerializer for serialization
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()  # Queryset for retrieving all books
+    serializer_class = BookSerializer  # Use the BookSerializer for serialization
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
